a0d86156b04ea2483cff11ad167553af7c337d52
Dana Hartweg <dana.hartweg@gmail.com>
2020-01-13 22:57:37 -0500

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/consume_firestore_lists/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.consume_firestore_lists
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
